e1f3108d29d134eebb01bf06e51af40eb611ab43
jtr354 <jtr354@foxmail.com>
2023-01-31 21:33:22 +0800

    feat: init color system

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   console.log(1234);
@@ -11,6 +11,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <h1>123</h1>
+        <h2>123</h2>
+        <h3>123</h3>
+        <hr />
+        <code>const a = 'b'</code>
         <a
           className="App-link"
           href="https://reactjs.org"
